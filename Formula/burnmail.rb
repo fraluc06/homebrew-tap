ca3f03fff12dc7ae211134cb844fac91edb54dd9
Francesco Lucarelli <67560467+fraluc06@users.noreply.github.com>
2025-12-24 19:36:17 +0000

    burnmail: add 1.2.2 bottle.

--- Formula/burnmail.rb
+++ Formula/burnmail.rb
@@ -6,6 +6,14 @@ class Burnmail < Formula
   license "MIT"
   head "https://github.com/fraluc06/burnmail.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/fraluc06/homebrew-tap/releases/download/burnmail-1.2.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7b0cd470bc97f8ed46181a1fc175684d9663f9f26269af63fc6c264de4055560"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "607dba5251145ac4cccb4f08a9b8cb94eac509ccc14cff3492f57cbe74708640"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a3c8160686790a7ca69759339900a68dad63691d2536d2cd43d800b177bc1b6f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f62c9e7b99b1d21581f52db21418553856c5566dfb3ba7a3772a23c74939496"
+  end
+
   depends_on "go" => :build
 
   def install
