fd384b8dd547daed7b17ad8fd3baf933c4ca01f3
Francesco Lucarelli <67560467+fraluc06@users.noreply.github.com>
2025-12-24 20:48:22 +0100

    burnmail 1.2.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/burnmail.rb
+++ Formula/burnmail.rb
@@ -1,7 +1,7 @@
 class Burnmail < Formula
   desc "Simple CLI tool to generate and manage temporary email addresses with TUI"
   homepage "https://github.com/fraluc06/burnmail"
-  url "https://github.com/fraluc06/burnmail/archive/refs/tags/v1.2.1.tar.gz"
+  url "https://github.com/fraluc06/burnmail/archive/refs/tags/v1.2.2.tar.gz"
   sha256 "dbc7df4f2d1727f94eee0b02b61b8fe39d9348b6db1939c165e30049bb798d5b"
   license "MIT"
   head "https://github.com/fraluc06/burnmail.git", branch: "main"
