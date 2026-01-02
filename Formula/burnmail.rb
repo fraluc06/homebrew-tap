f56f32d9c18d537b207160c2ed3412006bc2be31
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2026-01-02 21:31:27 +0100

    Add Homebrew formulae for Burnmail and Mkvtea, update CI configurations

---
+++ Formula/burnmail.rb
@@ -0,0 +1,23 @@
+class Burnmail < Formula
+  desc "Simple CLI tool to generate and manage temporary email addresses with TUI"
+  homepage "https://github.com/fraluc06/burnmail"
+  url "https://github.com/fraluc06/burnmail/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "184ef2c1990c6b4dcabebaac650ae9dc5d6a1bbbd6084e1247f274681fdc2fb5"
+  license "MIT"
+  head "https://github.com/fraluc06/burnmail.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags", "-s -w", "-o", bin/"burnmail"
+  end
+
+  test do
+    assert_match "burnmail", shell_output("#{bin}/burnmail version")
+  end
+end
---
+++ Formula/mkvtea.rb
@@ -0,0 +1,22 @@
+class Mkvtea < Formula
+  desc "Blazing-fast batch processing tool for managing anime/TV series MKV libraries"
+  homepage "https://github.com/fraluc06/mkvtea"
+  url "https://github.com/fraluc06/mkvtea/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "4c71bd3d6ce708d154114135f1eba9d4623cbc917cceaf1d6423b2b9cf7936a4"
+  license "AGPL-3.0-or-later"
+  
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+  depends_on "go" => :build
+  depends_on "mkvtoolnix"
+
+  def install
+    system "go", "build", "-ldflags", "-s -w", "-o", bin/"mkvtea"
+  end
+
+  test do
+    assert_match "mkvtea", shell_output("#{bin}/mkvtea --version")
+  end
+end
