c2b16bb18bf3d38fc6dcad8b91a6df46310b0586
Francesco Lucarelli <67560467+fraluc06@users.noreply.github.com>
2026-01-02 21:14:27 +0000

    mkvtea: add 0.1.2 bottle.

--- Formula/mkvtea.rb
+++ Formula/mkvtea.rb
@@ -10,6 +10,14 @@ class Mkvtea < Formula
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
+
+  bottle do
+    root_url "https://github.com/fraluc06/homebrew-tap/releases/download/mkvtea-0.1.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "996b19c0798c90b1100699f6c3bbc058eecf778409e9c9cc85c447ace7256534"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "061e98b3e51e475172eb5619f66c5bb841e7bdb266e4f10fc70006da8a99c428"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f6dabc5bf9d84682fdbb4c7753fbe3cb691184fc43754943ebbe5965f2fef827"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04e4aa170dd5b57e2cc023561023c3b8a5fba0194bbecd4b690d9eeeac35c3fb"
+  end
   depends_on "go" => :build
   depends_on "mkvtoolnix"
 
