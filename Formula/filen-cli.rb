8879c6b3278cb5424f8e6d8a403e8b8a0349799b
Francesco Lucarelli <67560467+fraluc06@users.noreply.github.com>
2026-03-08 10:41:53 +0000

    filen-cli: add 0.2.5 bottle.

--- Formula/filen-cli.rb
+++ Formula/filen-cli.rb
@@ -12,6 +12,14 @@ class FilenCli < Formula
     regex(/^filen-cli@v?(\d+\.\d+\.\d+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/fraluc06/homebrew-tap/releases/download/filen-cli-0.2.5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "efe1c85a5db658520cfa83019f74b7096b9cbbb537cc7e93495e52f9dbaf3c32"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da0dff4b3ec62a72c8baca14b8bc22652b211a83b8a1be93afa715662707892e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a0bdda67295967b8ef304987d866616d723ee9e3fcc66b59b500f8f36f63ab2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c89e9930e13f90a67a2423d69e4b2fc78fc000caf7b7ac1adf269d39d658fdd3"
+  end
+
   # cmake is required by heif-decoder/build.rs to compile libde265 and libheif
   depends_on "cmake" => :build
   # rustup is needed to install the nightly toolchain required by the project
