ade5e238a97a9d65d40cc708b2f645b2d566b68e
Francesco Lucarelli <67560467+fraluc06@users.noreply.github.com>
2026-04-20 19:37:09 +0000

    mkvtea: update 1.1.0 bottle.

--- Formula/mkvtea.rb
+++ Formula/mkvtea.rb
@@ -12,11 +12,11 @@ class Mkvtea < Formula
   end
 
   bottle do
-    root_url "https://github.com/fraluc06/homebrew-tap/releases/download/mkvtea-1.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a53cd268ef04f14c640d3ef5377eba42cb558e98ea9a7f70d67de9dd12a3433a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ba7f0f66048362cf48f441ad91ee9f09a6104c8d4da6549ec286d114b839266"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "273cc6efd158efb2e2317f4bcf4d77a34399de8cc2c4b97249298eeb0c5af438"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eebc8ee78cc97b2a803a1a91758aefbe949ca25beae1cf3fe88c8b71054d43a1"
+    root_url "https://github.com/fraluc06/homebrew-tap/releases/download/mkvtea-1.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "44e5afccfb0c94b540f03c4acdd30cb04474b997d759bccd440efc2f168a19bd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5270cb476c40954a38074c399506eb431cbc2129b6e6632c10a337f36f36f55d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "27cd0d86dbaee66479d839a23394a8b856c405b8e7f489c52c4679bfcafdc3fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df665d1716f386c6be916ee727edb705083b401932f16b580ddb0ce8c8d62f8c"
   end
   depends_on "go" => :build
   depends_on "mkvtoolnix"
